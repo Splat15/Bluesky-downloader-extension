5a66e90b6090f3a8d96301f1bcd14de76e6305b2
Splat <julianbrandau7@gmail.com>
2026-02-16 21:34:21 +0100

    Overhauled post info discovery

--- js/document.js
+++ js/document.js
@@ -1,10 +1,14 @@
 // This document runs on the main thread of the website to access things that the normal thread can't
 
-/** Gets base post element and at uri */
+/** Gets at uri */
 function GetURI(element) {
       let uri
+      let postInfo
       // Get base post element
-      let postElement = OuterQuerySelector(element, "div[data-testid*='postThreadItem-by-']")
+      let postElement = OuterQuerySelector(element, ["[data-testid*='Screen']>div>div>div>div>div", "div:has(>div>[data-testid*='feedItem-by'])"]).lastElementChild.lastElementChild
+      console.log(postElement)
+      postElement.style.border = "solid green 1px"
+
       // Get property keys
       const keys = Object.getOwnPropertyNames(postElement)
 
@@ -13,28 +17,83 @@ function GetURI(element) {
             for (let i = 0; i < keys.length; i++) {
                   const key = keys[i]
                   if (key.startsWith("__reactProps")) {
-                        try {
-                              // Try to return URI
-                              uri = postElement[key].children[0].props.item.uri
+                        let result = ScanObject(postElement[key], (element, path) => {
+                              return (/*path.endsWith("key") ||*/ path.endsWith("post.uri")) && element.startsWith("at://")
+
+                        }, 20).result
+                        if (result.hasOwnProperty("key")) {
+                              uri = result.key.replace(/lineartop$/i, "")
                         }
-                        catch {
-                              // Post is focused
-                              // URI is in document.URL
+                        else {
+                              postInfo = result
                         }
                         break
                   }
             }
-      if (!uri) uri = "none"
-      return uri
+
+      console.log(uri, postInfo)
+
+      return { uri: uri, postInfo: postInfo }
 }
 
 
 /** Like querySelector but working outwards through parents */
-function OuterQuerySelector(element, selector) {
-      while (!element.matches(selector)) {
+function OuterQuerySelector(element, selectors) {
+      while (!selectors.find(selector => element.matches(selector))) {
             if (element == document.body)
                   return false
             element = element.parentElement
       }
       return element
 }
+
+
+/**Scan object by specifying desired path with dot notation. 
+ * Will return element and its path */
+function ScanObject(object_, test, maxDepth, path = null, depth = 0, knownElements = []) {
+      let resultElem;
+      let resultPath;
+
+      // Depth is not too high
+      if (depth <= maxDepth) {
+            //console.log(path)
+            const keys = Object.keys(object_)
+            //console.log(keys)
+
+            for (let i = 0; i < keys.length; i++) {
+                  try {
+                        const child = object_[keys[i]]
+
+                        // If child is unknown
+                        if (child && !knownElements.includes(child)) {
+                              knownElements.push(child)
+
+                              let childPath = (path ? path + "." : "") + keys[i]
+                              if (test(child, childPath)) {
+                                    resultElem = object_
+                                    resultPath = childPath
+                                    break
+                              }
+
+                              if (typeof child !== "string") {
+                                    let results = ScanObject(child, test, maxDepth, childPath, depth + 1, knownElements)
+                                    knownElements = results.knownElements
+                                    resultElem = results.result
+                                    resultPath = results.path
+                                    if (resultElem) break
+                              }
+                        }
+                  } catch (e) {
+                        console.log(e)
+                  }
+            }
+      }
+
+      return {
+            knownElements: knownElements,
+            result: resultElem,
+            path: resultPath
+      }
+}
+
+throw new DOMException("apkjsoidh")
