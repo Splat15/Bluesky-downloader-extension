8965c3b93ffcbcacdde57f1cb270f65365f49bd3
Splat <julianbrandau7@gmail.com>
2026-02-20 00:26:34 +0100

    Refined post info fetching

--- js/document.js
+++ js/document.js
@@ -4,6 +4,7 @@
 function GetURI(element) {
       let uri
       let postInfo
+      let resultPath
       // Get base post element
       let postElement = OuterQuerySelector(element, ["[data-testid*='Screen']>div>div>div>div>div", "div:has(>div>[data-testid*='feedItem-by'])"]).lastElementChild.lastElementChild
       console.log(postElement)
@@ -18,10 +19,21 @@ function GetURI(element) {
                   const key = keys[i]
                   if (key.startsWith("__reactProps")) {
                         let result = ScanObject(postElement[key], (element, path) => {
-                              return (/*path.endsWith("key") ||*/ path.endsWith("post.uri")) && element.startsWith("at://")
-
-                        }, 20).result
-                        if (result.hasOwnProperty("key")) {
+                              return (path.endsWith("key") || path.endsWith("post.uri")) && element.startsWith("at://")
+                        }, 20,
+                              (resultElem, resultPath, stopSearch) => {
+                                    if (resultPath.endsWith("uri")) {
+                                          stopSearch()
+                                          console.log("uri found")
+                                    }
+                                    else {
+                                          console.log("key found, continuing")
+                                    }
+                              }
+                        )
+                        resultPath = result.path /*+ " (" + (Array.from(result.path.matchAll(/\./gi)).length + 1) + ")"*/
+                        result = result.result
+                        if (result && result.hasOwnProperty("key")) {
                               uri = result.key.replace(/lineartop$/i, "")
                         }
                         else {
@@ -31,7 +43,7 @@ function GetURI(element) {
                   }
             }
 
-      console.log(uri, postInfo)
+      console.log(uri, postInfo, resultPath)
 
       return { uri: uri, postInfo: postInfo }
 }
@@ -50,15 +62,14 @@ function OuterQuerySelector(element, selectors) {
 
 /**Scan object by specifying desired path with dot notation. 
  * Will return element and its path */
-function ScanObject(object_, test, maxDepth, path = null, depth = 0, knownElements = []) {
+function ScanObject(object_, test, maxDepth, onResult = null, path = null, depth = 0, knownElements = []) {
       let resultElem;
       let resultPath;
+      let stopped = false
 
       // Depth is not too high
       if (depth <= maxDepth) {
-            //console.log(path)
             const keys = Object.keys(object_)
-            //console.log(keys)
 
             for (let i = 0; i < keys.length; i++) {
                   try {
@@ -66,21 +77,32 @@ function ScanObject(object_, test, maxDepth, path = null, depth = 0, knownElemen
 
                         // If child is unknown
                         if (child && !knownElements.includes(child)) {
-                              knownElements.push(child)
+                              if(typeof child != "string") knownElements.push(child)
 
+                              // Join child path
                               let childPath = (path ? path + "." : "") + keys[i]
+
+                              // Test against supplied function
                               if (test(child, childPath)) {
                                     resultElem = object_
                                     resultPath = childPath
-                                    break
+
+                                    // Allow optional early termination after finding a match
+                                    let stopSearch = () => { stopped = true }
+                                    if (onResult)
+                                          onResult(resultElem, resultPath, stopSearch)
+                                    if (stopped) break
                               }
 
+                              // Process child
                               if (typeof child !== "string") {
-                                    let results = ScanObject(child, test, maxDepth, childPath, depth + 1, knownElements)
+                                    let results = ScanObject(child, test, maxDepth, onResult, childPath, depth + 1, knownElements)
                                     knownElements = results.knownElements
                                     resultElem = results.result
                                     resultPath = results.path
-                                    if (resultElem) break
+                                    stopped = results.stopped
+
+                                    if (stopped) break
                               }
                         }
                   } catch (e) {
@@ -92,7 +114,8 @@ function ScanObject(object_, test, maxDepth, path = null, depth = 0, knownElemen
       return {
             knownElements: knownElements,
             result: resultElem,
-            path: resultPath
+            path: resultPath,
+            stopped: stopped
       }
 }
 
